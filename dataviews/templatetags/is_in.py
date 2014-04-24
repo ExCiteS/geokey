ff77ea31730360d61ebbe9eef7bd0ddfc76e4644
Oliver Roick <oliver.roick@gmail.com>
2014-04-24 11:31:20 +0100

    Fixed bugs related rule template rendering

--- dataviews/templatetags/is_in.py
+++ dataviews/templatetags/is_in.py
@@ -6,8 +6,9 @@ register = template.Library()
 
 @register.simple_tag
 def is_in(value, key, d):
-    l = json.loads(d[key])
-    if str(value) in l:
-        return 'selected'
-    else:
-        return ''
+    if d.get(key) is not None:
+        l = json.loads(d[key])
+        if str(value) in l:
+            return 'selected'
+
+    return ''
--- dataviews/templatetags/key.py
+++ dataviews/templatetags/key.py
@@ -6,22 +6,27 @@ register = template.Library()
 
 @register.filter(name='key')
 def key(d, key_name):
-    return d[key_name]
+    if key_name in d:
+        return 'value=%s' % d[key_name]
+
+    return ''
 
 
 @register.filter(name='minval')
 def minval(d, key_name):
-    minval = json.loads(d[key_name])['minval']
-    if minval is not None:
-        return 'value=%s' % minval
-    else:
-        return ''
+    if d.get(key_name) is not None:
+        minval = json.loads(d.get(key_name)).get('minval')
+        if minval is not None:
+            return 'value=%s' % minval
+
+    return ''
 
 
 @register.filter(name='maxval')
 def maxval(d, key_name):
-    maxval = json.loads(d[key_name])['maxval']
-    if minval is not None:
-        return 'value=%s' % maxval
-    else:
-        return ''
+    if d.get(key_name) is not None:
+        maxval = json.loads(d.get(key_name)).get('maxval')
+        if maxval is not None:
+            return 'value=%s' % maxval
+
+    return ''
