bfba5a7cb89929c550017d117da07b53e8f9406b
Oliver Roick <oliver.roick@gmail.com>
2014-05-20 17:22:17 +0100

    Add and remove users from admins in UI

--- static/js/admin.ui.usergroup.js
+++ static/js/admin.ui.usergroup.js
@@ -6,193 +6,187 @@
  * ***********************************************/
 
 (function (global) {
-	'use strict';
+    'use strict';
 
-	/**
-	 * Constructor
-	 * @param {[type]} panelId   The HTML element id of the usergroup ui
-	 * @param {[type]} projectId The project id of the usergroup
-	 * @param {[type]} viewId    Optional. The view id of the usergroup
-	 */
-	function Usergroup(panelId, projectId, viewId) {
-		var panel = $(panelId);
-		var groupId = panel.attr('data-group-id');
+    var panel = $('#usergroup-users');
+    var projectId = $('body').attr('data-project-id');
+    var groupId = panel.attr('data-group-id');
 
-		// Initializes the message display
-		this.messages = new Ui.MessageDisplay();
+    // Initializes the message display
+    var messages = new Ui.MessageDisplay();
 
-		// Get the elements
-		this.header = panel.find('.panel-heading');
-		this.typeAwayResults = panel.find('.panel-footer .type-away');
-		this.formField = panel.find('.panel-footer input[type="text"]');
-		this.userList = panel.find('.user-list');
+    // Get the elements
+    var header = panel.find('.panel-heading');
+    var typeAwayResults = panel.find('.panel-footer .type-away');
+    var formField = panel.find('.panel-footer input[type="text"]');
+    var userList = panel.find('.user-list');
 
-		// Build the request url
-		this.url = 'projects/' + projectId + '/usergroups/' + groupId + '/users';
-		if (viewId) { this.url = 'projects/' + projectId + '/views/' + viewId + '/usergroups/' + groupId + '/users'; }
+    var url;
+    if (groupId) {
+        url = 'projects/' + projectId + '/usergroups/' + groupId + '/users/';
+    } else {
+        url = 'projects/' + projectId + '/admins/';
+    }
 
-		this.numberOfRequests = 0;
+    var numberOfRequests = 0;
 
-		// Register the click event handler of remove links
-		this.userList.find('li a.remove').click(this.handleRemoveUser.bind(this));
+    function displaySuccess() {
+        header.removeClass('loading');
+        messages.showInlineSuccess(header);
+    }
 
-		// Register the keyup event on the text field.
-		this.formField.keydown(this.handleFormType.bind(this));
-	}
+    function displayError(msg) {
+        header.removeClass('loading');
+        messages.showInlineError(header, msg);
+    }
 
-	Usergroup.prototype.displaySuccess = function displaySuccess() {
-		this.header.removeClass('loading');
-		this.messages.showInlineSuccess(this.header);
-	};
+    /**
+     * Handles the click event when the user clicks on the remove link in the user list.
+     * @param  {Event} event The click event fired by the link.
+     */
+    function handleRemoveUser(event) {
+        var userId = $(event.target).parent('a').attr('data-user-id');
+        var itemToRemove = $(event.target).parents('.list-group-item');
 
-	Usergroup.prototype.displayError = function displayError(msg) {
-		this.header.removeClass('loading');
-		this.messages.showInlineError(this.header, msg);
-	};
+        /**
+         * Handles the response after the removal of the user from the group was successful.
+         */
+        function handleRemoveUserSuccess() {
+            itemToRemove.remove();
+            if (userList.children().length === 0) {
+                userList.append('<li class="list-group-item">No users have been assigned to this group.</li>');
+            }
+            displaySuccess();
+        }
 
-	/**
-	 * Handles the click event when the user clicks on the remove link in the user list.
-	 * @param  {Event} event The click event fired by the link.
-	 */
-	Usergroup.prototype.handleRemoveUser = function handleRemoveUser(event) {
-		var userId = $(event.target).parent('a').attr('data-user-id');
-		var itemToRemove = $(event.target).parents('.list-group-item');
+        /**
+         * Handles the response after the removal of the user from the group failed.
+         * @param  {Object} response JSON object of the response
+         */
+        function handleRemoveUserError(response) {
+            displayError('An error occured while removing the user. Error text was: ' + response.responseJSON.error);
+        }
 
-		/**
-		 * Handles the response after the removal of the user from the group was successful.
-		 */
-		function handleRemoveUserSuccess() {
-			itemToRemove.remove();
-			if (this.userList.children().length === 0) {
-				this.userList.append('<li class="list-group-item">No users have been assigned to this group.</li>');
-			}
-			this.displaySuccess();
-		}
-
-		/**
-		 * Handles the response after the removal of the user from the group failed.
-		 * @param  {Object} response JSON object of the response
-		 */
-		function handleRemoveUserError(response) {
-			this.displayError('An error occured while removing the user. Error text was: ' + response.responseJSON.error);
-		}
-
-		Control.Ajax.del(this.url + '/' + userId, handleRemoveUserSuccess.bind(this), handleRemoveUserError.bind(this), {'userId': userId});
-		event.preventDefault();
-	};
+        Control.Ajax.del(url + userId+ '/', handleRemoveUserSuccess, handleRemoveUserError, {'userId': userId});
+        event.preventDefault();
+    }
 
 
-	/**
-	 * Handles the response if the addition of the user to the group was successful.
-	 * @param  {Object} response JSON object of the response
-	 */
-	Usergroup.prototype.handleAddUserSucess = function handleAddUserSucess(response) {
-		this.userList.empty();
-		this.userList.append(Templates.usergroupusers(response));
-		this.userList.find('li a.remove').click(this.handleRemoveUser.bind(this));
+    /**
+     * Handles the response if the addition of the user to the group was successful.
+     * @param  {Object} response JSON object of the response
+     */
+    function handleAddUserSucess(response) {
+        userList.empty();
+        userList.append(Templates.usergroupusers(response));
+        userList.find('li a.remove').click(handleRemoveUser);
 
-		this.formField.val('');
-		this.displaySuccess();
-	};
+        formField.val('');
+        displaySuccess();
+    }
 
-	/**
-	 * Handles the response if the addition of the user to the group failed.
-	 * @param  {Object} response JSON object of the response
-	 */
-	Usergroup.prototype.handleAddUserError = function handleAddUserError(response) {
-		this.displayError('An error occured while adding the user. Error text was: ' + response.responseJSON.error);
-	};
+    /**
+     * Handles the response if the addition of the user to the group failed.
+     * @param  {Object} response JSON object of the response
+     */
+    function handleAddUserError(response) {
+        displayError('An error occured while adding the user. Error text was: ' + response.responseJSON.error);
+    }
 
-	/**
-	 * Handles click events pn items in the user dropdown list. Adds the user to the group.
-	 * @param  {Event} event The click event on the user link.
-	 */
-	Usergroup.prototype.handleAddUser = function handleAddUser(event) {
-		var userId = $(event.target).attr('data-user-id');
+    /**
+     * Handles click events pn items in the user dropdown list. Adds the user to the group.
+     * @param  {Event} event The click event on the user link.
+     */
+    function handleAddUser(event) {
+        var userId = $(event.target).attr('data-user-id');
 
-		this.header.addClass('loading');
-		this.typeAwayResults.hide();
-		Control.Ajax.post(this.url, this.handleAddUserSucess.bind(this), this.handleAddUserError.bind(this), {'userId': userId});
-		event.preventDefault();
-	};
+        header.addClass('loading');
+        typeAwayResults.hide();
+        Control.Ajax.post(url, handleAddUserSucess, handleAddUserError, {'userId': userId});
+        event.preventDefault();
+    }
 
 
 
-	/**
-	 * Handles the reponse the the request for the user list was successful. Updates the dropdown list.
-	 * @param  {Object} response JSON object of the response
-	 */
-	Usergroup.prototype.handleSuccess = function handleSuccess(response) {
-		var users = response;
-		var header = (users.length > 0 ? 'Click on item to add user' : 'No records matched your query');
+    /**
+     * Handles the reponse the the request for the user list was successful. Updates the dropdown list.
+     * @param  {Object} response JSON object of the response
+     */
+    function handleSuccess(response) {
+        var users = response;
+        var header = (users.length > 0 ? 'Click on item to add user' : 'No records matched your query');
 
-		this.typeAwayResults.children().not('.dropdown-header').remove();
-		this.numberOfRequests--;
-		if (this.numberOfRequests === 0) {
-			this.formField.removeClass('loading');
-		}
+        typeAwayResults.children().not('.dropdown-header').remove();
+        numberOfRequests--;
+        if (numberOfRequests === 0) {
+            formField.removeClass('loading');
+        }
 
-		this.typeAwayResults.append(Templates.userstypeaway(response));
+        typeAwayResults.append(Templates.userstypeaway(response));
 
-		this.typeAwayResults.children('.dropdown-header').text(header);
-		this.typeAwayResults.find('li a').click(this.handleAddUser.bind(this));
-		this.typeAwayResults.show();
-	};
+        typeAwayResults.children('.dropdown-header').text(header);
+        typeAwayResults.find('li a').click(handleAddUser);
+        typeAwayResults.show();
+    }
 
-	/**
-	 * Handles the reponse the the request for the user list failed.
-	 */
-	Usergroup.prototype.handleError = function handleError() {
-		this.numberOfRequests--;
-		if (this.numberOfRequests === 0) {
-			this.formField.removeClass('loading');
-		}
-		this.typeAwayResults.children().not('.dropdown-header').remove();
-		this.typeAwayResults.children('.dropdown-header').html('<span class="text-danger">An error occured while trying to retrieve the user list. Please try again.</span>');
-		this.typeAwayResults.show();
-	};
+    /**
+     * Handles the reponse the the request for the user list failed.
+     */
+    function handleError() {
+        numberOfRequests--;
+        if (numberOfRequests === 0) {
+            formField.removeClass('loading');
+        }
+        typeAwayResults.children().not('.dropdown-header').remove();
+        typeAwayResults.children('.dropdown-header').html('<span class="text-danger">An error occured while trying to retrieve the user list. Please try again.</span>');
+        typeAwayResults.show();
+    }
 
-	/**
-	 * Handles user type in the search field. Requests for a users list after 3 characters have been typed.
-	 * @param  {Event} event The keyup event fired by the field.
-	 */
-	Usergroup.prototype.handleFormType = function handleFormType(event) {
-		var activeLink = this.typeAwayResults.find('li.active');
-		if (event.keyCode === 13) {
-			if (activeLink.length > 0) {
-				activeLink.children().click();
-			}
-		}
-		else if ((event.keyCode === 40 || event.keyCode === 38) && this.typeAwayResults.is(":visible")) {
-			event.preventDefault();
-			if (activeLink.length > 0) {
-				if (event.keyCode === 38) {
-					// move up
-					if (activeLink.prev().not('.dropdown-header').length) {
-						activeLink.removeClass('active');
-						activeLink.prev().addClass('active');
-					}
-				}
-				if (event.keyCode === 40) {
-					// move down
-					if (activeLink.next().length) {
-						activeLink.removeClass('active');
-						activeLink.next().addClass('active');
-					}
-				}
-			} else {
-				this.typeAwayResults.find('li').not('.dropdown-header').first().addClass('active');
-			}
-		} else {
-			if (event.target.value.length === 1) {
-				this.typeAwayResults.hide();
-			} else if (event.target.value.length >= 2) {
-				this.formField.addClass('loading');
-				this.numberOfRequests++;
-				Control.Ajax.get('users?query=' + event.target.value, this.handleSuccess.bind(this), this.handleError.bind(this));
-			}
-		}
-	};
+    /**
+     * Handles user type in the search field. Requests for a users list after 3 characters have been typed.
+     * @param  {Event} event The keyup event fired by the field.
+     */
+    function handleFormType(event) {
+        var activeLink = typeAwayResults.find('li.active');
+        if (event.keyCode === 13) {
+            if (activeLink.length > 0) {
+                activeLink.children().click();
+            }
+        }
+        else if ((event.keyCode === 40 || event.keyCode === 38) && typeAwayResults.is(":visible")) {
+            event.preventDefault();
+            if (activeLink.length > 0) {
+                if (event.keyCode === 38) {
+                    // move up
+                    if (activeLink.prev().not('.dropdown-header').length) {
+                        activeLink.removeClass('active');
+                        activeLink.prev().addClass('active');
+                    }
+                }
+                if (event.keyCode === 40) {
+                    // move down
+                    if (activeLink.next().length) {
+                        activeLink.removeClass('active');
+                        activeLink.next().addClass('active');
+                    }
+                }
+            } else {
+                typeAwayResults.find('li').not('.dropdown-header').first().addClass('active');
+            }
+        } else {
+            if (event.target.value.length === 1) {
+                typeAwayResults.hide();
+            } else if (event.target.value.length >= 2) {
+                formField.addClass('loading');
+                numberOfRequests++;
+                Control.Ajax.get('users?query=' + event.target.value, handleSuccess, handleError);
+            }
+        }
+    }
 
-	global.Usergroup = Usergroup;
-}(window.Ui ? window.Ui : window.Ui = {}));
+    // Register the click event handler of remove links
+    userList.find('li a.remove').click(handleRemoveUser);
+
+    // Register the keyup event on the text field.
+    formField.keydown(handleFormType);
+}());
