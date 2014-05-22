7fe8716cdab21de707fe8bc69a41d7d625040dc6
Oliver Roick <oliver.roick@gmail.com>
2014-05-22 15:44:11 +0100

    Moved project settings to sample file

--- core/settings/project.sample.py
+++ core/settings/project.sample.py
@@ -8,9 +8,9 @@ DATABASES = {
         'ENGINE': 'django.contrib.gis.db.backends.postgis', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'communitymaps',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'django',
-        'PASSWORD': 'django123',
-        'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'USER': '[YOUR DB USERNAME]',
+        'PASSWORD': '[YOUR DB USERNAME]',
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
 }
