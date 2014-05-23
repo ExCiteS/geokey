01dd5f25ae96e09ecefc154aa2e6e30ca35e826e
Oliver Roick <oliver.roick@gmail.com>
2014-05-23 14:17:56 +0100

    Automate field key
    Fixes #77

---
+++ static/js/admin.ui.field.create.js
@@ -0,0 +1,14 @@
+(function () {
+    'use strict';
+
+    function setFieldKey(event) {
+        var keyField = $('input#key');
+        var fieldName = $(event.target).val();
+        if (!keyField.val().length) {
+            var keyValue = fieldName.replace(/\s/g, '_').replace(/[^\w\s]/gi, '').toLowerCase();
+            keyField.val(keyValue);
+        }
+    }
+
+    $('input#name').blur(setFieldKey);
+}());
