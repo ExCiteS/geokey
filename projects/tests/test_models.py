640a8b792a6d07be997019e74d00470c1566e54b
Oliver Roick <oliver.roick@gmail.com>
2014-06-03 11:57:27 +0100

    Added more tests for Project model methods

--- projects/tests/test_models.py
+++ projects/tests/test_models.py
@@ -9,7 +9,17 @@ from .model_factories import UserF, ProjectF
 from ..models import Project
 
 
-class ProjectTest(TestCase):
+class CreateProjectTest(TestCase):
+    def test_create_project(self):
+        creator = UserF.create()
+
+        project = Project.create(
+            'Test', 'Test desc', True, creator
+        )
+        self.assertIn(creator, project.admins.all())
+
+
+class PrivateProjectTest(TestCase):
     def setUp(self):
         self.creator = UserF.create()
         self.admin = UserF.create()
@@ -22,14 +32,6 @@ class ProjectTest(TestCase):
             add_contributors=[self.contributor],
             add_viewers=[self.view_member]
         )
-
-    def test_create_project(self):
-        creator = UserF.create()
-
-        project = Project.create(
-            'Test', 'Test desc', True, creator
-        )
-        self.assertIn(creator, project.admins.all())
 
     @raises(Project.DoesNotExist)
     def test_delete_project(self):
@@ -54,6 +56,7 @@ class ProjectTest(TestCase):
         self.assertFalse(self.private_project.is_admin(self.contributor))
         self.assertFalse(self.private_project.is_admin(self.non_member))
         self.assertFalse(self.private_project.is_admin(self.view_member))
+        self.assertFalse(self.private_project.is_admin(AnonymousUser()))
 
     def test_can_contribute(self):
         self.assertTrue(self.private_project.can_contribute(self.admin))
@@ -61,6 +64,7 @@ class ProjectTest(TestCase):
         self.assertTrue(self.private_project.can_contribute(self.contributor))
         self.assertFalse(self.private_project.can_contribute(self.non_member))
         self.assertFalse(self.private_project.can_contribute(self.view_member))
+        self.assertFalse(self.private_project.can_contribute(AnonymousUser()))
 
     def test_can_access(self):
         self.assertTrue(self.private_project.can_access(self.admin))
@@ -68,6 +72,89 @@ class ProjectTest(TestCase):
         self.assertTrue(self.private_project.can_access(self.contributor))
         self.assertFalse(self.private_project.can_access(self.non_member))
         self.assertTrue(self.private_project.can_access(self.view_member))
+        self.assertFalse(self.private_project.can_access(AnonymousUser()))
+
+
+class PublicProjectTestNoPublicView(TestCase):
+    def setUp(self):
+        self.creator = UserF.create()
+        self.admin = UserF.create()
+        self.contributor = UserF.create()
+        self.non_member = UserF.create()
+        self.view_member = UserF.create()
+
+        self.project = ProjectF.create(
+            add_admins=[self.admin, self.creator],
+            add_contributors=[self.contributor],
+            add_viewers=[self.view_member],
+            **{'isprivate': False}
+        )
+
+    def test_is_admin(self):
+        self.assertTrue(self.project.is_admin(self.admin))
+        self.assertTrue(self.project.is_admin(self.creator))
+        self.assertFalse(self.project.is_admin(self.contributor))
+        self.assertFalse(self.project.is_admin(self.non_member))
+        self.assertFalse(self.project.is_admin(self.view_member))
+        self.assertFalse(self.project.is_admin(AnonymousUser()))
+
+    def test_can_contribute(self):
+        self.assertTrue(self.project.can_contribute(self.admin))
+        self.assertTrue(self.project.can_contribute(self.creator))
+        self.assertTrue(self.project.can_contribute(self.contributor))
+        self.assertFalse(self.project.can_contribute(self.non_member))
+        self.assertFalse(self.project.can_contribute(self.view_member))
+        self.assertFalse(self.project.can_contribute(AnonymousUser()))
+
+    def test_can_access(self):
+        self.assertTrue(self.project.can_access(self.admin))
+        self.assertTrue(self.project.can_access(self.creator))
+        self.assertTrue(self.project.can_access(self.contributor))
+        self.assertFalse(self.project.can_access(self.non_member))
+        self.assertTrue(self.project.can_access(self.view_member))
+        self.assertFalse(self.project.can_access(AnonymousUser()))
+
+
+class PublicProjectTestWithPublicView(TestCase):
+    def setUp(self):
+        self.creator = UserF.create()
+        self.admin = UserF.create()
+        self.contributor = UserF.create()
+        self.non_member = UserF.create()
+        self.view_member = UserF.create()
+
+        self.project = ProjectF.create(
+            add_admins=[self.admin, self.creator],
+            add_contributors=[self.contributor],
+            add_viewers=[self.view_member],
+            **{'isprivate': False}
+        )
+
+        ViewFactory(**{'isprivate': False, 'project': self.project})
+
+    def test_is_admin(self):
+        self.assertTrue(self.project.is_admin(self.admin))
+        self.assertTrue(self.project.is_admin(self.creator))
+        self.assertFalse(self.project.is_admin(self.contributor))
+        self.assertFalse(self.project.is_admin(self.non_member))
+        self.assertFalse(self.project.is_admin(self.view_member))
+        self.assertFalse(self.project.is_admin(AnonymousUser()))
+
+    def test_can_contribute(self):
+        self.assertTrue(self.project.can_contribute(self.admin))
+        self.assertTrue(self.project.can_contribute(self.creator))
+        self.assertTrue(self.project.can_contribute(self.contributor))
+        self.assertFalse(self.project.can_contribute(self.non_member))
+        self.assertFalse(self.project.can_contribute(self.view_member))
+        self.assertFalse(self.project.can_contribute(AnonymousUser()))
+
+    def test_can_access(self):
+        self.assertTrue(self.project.can_access(self.admin))
+        self.assertTrue(self.project.can_access(self.creator))
+        self.assertTrue(self.project.can_access(self.contributor))
+        self.assertTrue(self.project.can_access(self.non_member))
+        self.assertTrue(self.project.can_access(self.view_member))
+        self.assertTrue(self.project.can_access(AnonymousUser()))
 
 
 class ProjectIsInvolved(TestCase):
@@ -106,7 +193,7 @@ class ProjectIsInvolved(TestCase):
             **{'isprivate': False}
         )
 
-        ViewFactory(**{'isprivate': False})
+        ViewFactory(**{'isprivate': False, 'project': self.project})
 
         self.assertTrue(self.project.is_involved(self.admin))
         self.assertTrue(self.project.is_involved(self.creator))
