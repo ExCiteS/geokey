6b92ad48595aa091672ffaeb012d98817c808184
Oliver Roick <oliver.roick@gmail.com>
2014-07-04 14:06:55 +0100

    Added additional status options for observations
    - draft
    - pending
    - suspended

--- contributions/base.py
+++ contributions/base.py
@@ -1,5 +1,7 @@
 from model_utils import Choices
 
 LOCATION_STATUS = Choices('active', 'review')
-OBSERVATION_STATUS = Choices('active', 'review', 'deleted')
+OBSERVATION_STATUS = Choices(
+    'active', 'draft', 'review', 'pending', 'suspended', 'deleted'
+)
 COMMENT_STATUS = Choices('active', 'deleted')
