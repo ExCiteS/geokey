8bd75506f9a56043cb8805eb6607470dccbbae5e
Oliver Roick <oliver.roick@gmail.com>
2014-07-22 14:52:00 +0100

    DateTime picker now handles 24h times
    Fixes #120

--- dataviews/templatetags/key.py
+++ dataviews/templatetags/key.py
@@ -17,7 +17,7 @@ def minval(d, key_name):
     if d.get(key_name) is not None:
         minval = json.loads(d.get(key_name)).get('minval')
         if minval is not None:
-            return 'value=%s' % minval
+            return minval
 
     return ''
 
@@ -27,6 +27,6 @@ def maxval(d, key_name):
     if d.get(key_name) is not None:
         maxval = json.loads(d.get(key_name)).get('maxval')
         if maxval is not None:
-            return 'value=%s' % maxval
+            return maxval
 
     return ''
