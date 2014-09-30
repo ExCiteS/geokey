2036eef22b51a5162ec70c922574a1f367fad1fa
Oliver Roick <oliver.roick@gmail.com>
2014-09-30 17:30:14 +0100

    Front-end integration of field sorting

--- static/js/admin.ui.category.display.js
+++ static/js/admin.ui.category.display.js
@@ -1,17 +1,5 @@
 $(function () {
     var symbolPath = $('body').attr('data-symbol')
-
-    // $("input#colour").pickAColor({
-    //     showSpectrum            : true,
-    //     // showSavedColors         : false,
-    //     // saveColorsPerElement    : false,
-    //     // fadeMenuToggle          : false,
-    //     // showHexInput            : false,
-    //     // showBasicColors         : false,
-    //     // allowBlank              : false,
-    //     // inlineDropdown          : false     
-    // });
-    // 
     $("#colour").colorpicker();
 
     var fileinputConfig = {
