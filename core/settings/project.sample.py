68934450257da0ad2f06dc8a32fff8a4cc606a0f
Oliver Roick <oliver.roick@gmail.com>
2014-10-08 17:17:10 +0100

    Update project.sample.py

--- core/settings/project.sample.py
+++ core/settings/project.sample.py
@@ -11,7 +11,7 @@ DATABASES = {
         'NAME': 'communitymaps',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '[YOUR DB USERNAME]',
-        'PASSWORD': '[YOUR DB USERNAME]',
+        'PASSWORD': '[YOUR DB PASSWORD]',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
