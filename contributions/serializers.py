634b702271cd3da1b25ad28832efb706d7716014
Oliver Roick <oliver.roick@gmail.com>
2014-10-24 11:33:37 +0100

    Remove lower case sensibility from search result serialisation

--- contributions/serializers.py
+++ contributions/serializers.py
@@ -207,13 +207,13 @@ class ContributionSerializer(object):
             'isowner': obj.creator == self.context.get('user')
         }
 
-        q = self.context.get('search')
+        q = self.context.get('search').lower()
         if q is not None:
             json_object['search_matches'] = {}
             matcher = obj.search_matches.split('#####')
 
             for field in matcher:
-                if q in field:
+                if q in field.lower():
                     match = field.split(':', 1)
                     json_object['search_matches'][match[0]] = match[1]
 
