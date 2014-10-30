76990898e7af3bb625792c6e8160e421b95d967a
Julius Osokinas <josokinas@users.noreply.github.com>
2014-10-30 11:47:18 +0000

    Added new video formats

--- contributions/base.py
+++ contributions/base.py
@@ -7,5 +7,5 @@ OBSERVATION_STATUS = Choices(
 COMMENT_STATUS = Choices('active', 'deleted')
 
 ACCEPTED_IMAGE_FORMATS = ('png', 'jpeg', 'gif')
-ACCEPTED_VIDEO_FORMATS = ('mpeg', 'mp4', 'quicktime', 'x-msvideo', '3gpp',
-                          'x-flv', 'x-ms-wmv')
+ACCEPTED_VIDEO_FORMATS = ('mpeg', 'mpeg2', 'mp4', 'quicktime', '3gpp', '3gpp2',
+                          'avi', 'x-flv', 'x-msvideo', 'x-ms-wmv', 'ogg', 'webm')
