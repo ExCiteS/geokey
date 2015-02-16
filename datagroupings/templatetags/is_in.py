5676d5a7ca38b531ff84a5b26974d5385da3ca65
Oliver Roick <oliver.roick@gmail.com>
2015-02-16 17:31:26 +0000

    Fixes #223

--- datagroupings/templatetags/is_in.py
+++ datagroupings/templatetags/is_in.py
@@ -6,9 +6,10 @@ register = template.Library()
 
 @register.simple_tag
 def is_in(value, key, d):
-    if d.get(key) is not None:
-        l = json.loads(d[key])
-        if str(value) in l:
-            return 'selected'
+    if d is not None:
+        if d.get(key) is not None:
+            l = json.loads(d[key])
+            if str(value) in l:
+                return 'selected'
 
     return ''
