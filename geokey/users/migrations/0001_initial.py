9cee82b4cd2e36dbfab054716dab3112cf158081
Oliver Roick <oliver.roick@gmail.com>
2015-07-10 09:40:20 +0100

    Fix migrations

--- geokey/users/migrations/0001_initial.py
+++ geokey/users/migrations/0001_initial.py
@@ -9,7 +9,6 @@ from django.conf import settings
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('datagroupings', '0001_initial'),
         ('projects', '0001_initial'),
     ]
 
@@ -32,18 +31,6 @@ class Migration(migrations.Migration):
             bases=(models.Model,),
         ),
         migrations.CreateModel(
-            name='GroupingUserGroup',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('can_read', models.BooleanField(default=True)),
-                ('can_view', models.BooleanField(default=True)),
-                ('grouping', models.ForeignKey(related_name='usergroups', to='datagroupings.Grouping')),
-            ],
-            options={
-            },
-            bases=(models.Model,),
-        ),
-        migrations.CreateModel(
             name='UserGroup',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
@@ -57,11 +44,5 @@ class Migration(migrations.Migration):
             options={
             },
             bases=(models.Model,),
-        ),
-        migrations.AddField(
-            model_name='groupingusergroup',
-            name='usergroup',
-            field=models.ForeignKey(related_name='viewgroups', to='users.UserGroup'),
-            preserve_default=True,
         ),
     ]
