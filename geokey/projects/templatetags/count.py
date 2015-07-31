d500d72b141dc6f7ac4be2abb3ff497c1bc51a63
Oliver Roick <oliver.roick@gmail.com>
2015-07-31 10:49:36 +0100

    Minor fixes to project template

--- geokey/projects/templatetags/count.py
+++ geokey/projects/templatetags/count.py
@@ -6,3 +6,8 @@ register = template.Library()
 @register.simple_tag
 def minus(count, to_sub):
     return count - to_sub
+
+
+@register.simple_tag
+def plus(count, to_add):
+    return count + to_add
