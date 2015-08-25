acd7673c75e518ef7442e9e6ab6d4fadb46eb539
Oliver Roick <oliver.roick@gmail.com>
2015-08-25 17:13:13 +0100

    Adds custom template tag for project info
    The tag shows if project is private, archived, who created it and when.

--- geokey/projects/tests/test_templatetags.py
+++ geokey/projects/tests/test_templatetags.py
@@ -1,8 +1,13 @@
+import datetime
+
 from django.test import TestCase
-from ..templatetags import count
+from ..templatetags import count, project_attributes
+
+from geokey.users.models import User
+from geokey.projects.models import Project
 
 
-class TemplateTagTest(TestCase):
+class CountTest(TestCase):
     def test_more_link_text(self):
         self.assertEqual(
             count.more_link_text(6, 'category', 'categories'),
@@ -23,3 +28,43 @@ class TemplateTagTest(TestCase):
             count.more_link_text(6, 'category', 'categories', 4),
             'Show 2 more categories'
         )
+
+
+class ProjectAttributesTest(TestCase):
+    def test_project_attributes(self):
+        user = User(display_name='Test Name')
+        project = Project(
+            isprivate=True,
+            creator=user,
+            created_at=datetime.datetime.now(),
+            status='active'
+        )
+        html = project_attributes.project_attributes(project)
+
+        self.assertIn(
+            ('Created by %s on %s') % (
+                user.display_name,
+                project.created_at.strftime("%d %B %Y, %H:%M")
+            ),
+            html
+        )
+        self.assertIn('<span class="label label-primary">Private</span>', html)
+        self.assertNotIn('Archived', html)
+
+        project = Project(
+            isprivate=False,
+            creator=user,
+            created_at=datetime.datetime.now(),
+            status='inactive'
+        )
+        html = project_attributes.project_attributes(project)
+
+        self.assertIn(
+            ('Created by %s on %s') % (
+                user.display_name,
+                project.created_at.strftime("%d %B %Y, %H:%M")
+            ),
+            html
+        )
+        self.assertIn('<span class="label label-primary">Public</span>', html)
+        self.assertIn('<span class="label label-warning">Archived</span>', html)
