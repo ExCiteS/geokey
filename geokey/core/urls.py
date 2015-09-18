b1d9f1dd2bf404e1bc2685297f48d093d3ed3913
Oliver Roick <oliver.roick@gmail.com>
2015-09-18 11:23:11 +0100

    Update requirements

--- geokey/core/urls.py
+++ geokey/core/urls.py
@@ -10,7 +10,6 @@ urlpatterns = patterns(
     url(r'^api/', include('geokey.core.url.api', namespace="api")),
     url(r'^oauth2/', include('oauth2_provider.urls', namespace='oauth2_provider')),
     url(r'^admin/account/', include('allauth.urls')),
-    # url(r'^', include('core.url.extensions')),
     url(r'^$', RedirectView.as_view(url='/admin/', permanent=True)),
 )
 
