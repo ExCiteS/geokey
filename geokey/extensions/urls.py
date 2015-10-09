ce1d3b5ceb920950f7d04b51a6232fc1076e152a
Julius O <j.osokinas@mappingforchange.org.uk>
2015-10-09 16:53:36 +0100

    Includes urls of extension only when showing interface
    Fixes #311.

--- geokey/extensions/urls.py
+++ geokey/extensions/urls.py
@@ -5,10 +5,7 @@ from geokey.extensions.base import extensions
 
 urlpatterns = []
 
-for extension in extensions:
-    try:
+for extension, settings in extensions.iteritems():
+    if settings['display_admin']:
         urls = '%s.urls' % extension
-        __import__(urls)
         urlpatterns.append(url(r'^', include(urls, namespace=extension)))
-    except ImportError:
-        pass
