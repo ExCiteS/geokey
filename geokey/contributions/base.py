902cf2df9e26f2960948406af6405740085b813c
Matthias Stevens <matthias.stevens@gmail.com>
2016-01-18 14:58:52 +0100

    Added 'mp3' as accepted audio format again
    Normally 'mpeg' should have been enough since 'audio/mpeg' is the recommended content-type for MP3 files, but it seems like some browsers (notably Chrome) use 'audio/mp3' instead
    See: http://stackoverflow.com/a/28021591/1084488
    
    Signed-off-by: Matthias Stevens <matthias.stevens@gmail.com>

--- geokey/contributions/base.py
+++ geokey/contributions/base.py
@@ -9,7 +9,7 @@ COMMENT_REVIEW = Choices('open', 'resolved')
 MEDIA_STATUS = Choices('active', 'deleted')
 
 ACCEPTED_IMAGE_FORMATS = ('png', 'jpeg', 'gif')
-ACCEPTED_AUDIO_FORMATS = ('wav', 'wave', 'mpeg', '3gpp', '3gpp2')
+ACCEPTED_AUDIO_FORMATS = ('wav', 'wave', 'mp3', 'mpeg', '3gpp', '3gpp2')
 ACCEPTED_VIDEO_FORMATS = ('mpeg', 'mpeg2', 'mp4', 'quicktime', '3gpp', '3gpp2',
                           'avi', 'x-flv', 'x-matroska', 'x-msvideo',
                           'x-ms-wmv', 'ogg', 'webm')
