f209641aa976eef82bb9098081abb75fb9f3ae19
Julius O <j.osokinas@mappingforchange.org.uk>
2016-01-27 12:03:40 +0000

    Further improvements to templates
    This is due to 0.9 release.

--- geokey/static/js/admin.ui.usergroup.permissions.js
+++ geokey/static/js/admin.ui.usergroup.permissions.js
@@ -18,7 +18,7 @@
             if (projectPrivate !== 'True' && everyoneContributes === 'auth') {
                 add = ', who have access to this project,';
             }
-            $('form#permissions').before('<div class="alert alert-warning hint"><strong>Note:</strong> Currently, all users' + add + ' can contribute to the project. This setting overwrites permissions of individual user groups. If you plan to restrict contributing permissions to certain user groups, head to <a href="/admin/projects/' + projectId + '/settings/" class="alert-link">Project Settings</a> first and change the project permissions. </div>');
+            $('form#permissions').before('<div class="alert alert-warning hint">Currently, all users' + add + ' can contribute to the project. This setting overwrites permissions of individual user groups. If you plan to restrict contributing permissions to certain user groups, head to <a href="/admin/projects/' + projectId + '/settings/" class="alert-link">project settings</a> first and change the project permissions. </div>');
         } else {
             $('.hint').remove();
         }
