5913e9e7d1f2dac286a0711ea3d027ada5a7d4c6
Julius O <j.osokinas@mappingforchange.org.uk>
2016-04-01 17:00:40 +0100

    Fixed redirection of index page
    This solves uwsgi issue.

--- geokey/core/urls.py
+++ geokey/core/urls.py
@@ -10,8 +10,8 @@ urlpatterns = [
     url(r'^api/', include('geokey.core.url.api', namespace="api")),
     url(r'^oauth2/', include('oauth2_provider.urls', namespace='oauth2_provider')),
     url(r'^admin/account/', include('allauth.urls')),
-    url(r'^$', RedirectView.as_view(url='/admin/', permanent=True)),
     url(r'^', include('geokey.extensions.urls')),
+    url(r'$', RedirectView.as_view(url='/admin/', permanent=True)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
