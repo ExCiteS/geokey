c40d62a4b6baf5201677757195c07917693d3cc3
Julius O <j.osokinas@mappingforchange.org.uk>
2016-04-19 16:59:27 +0100

    Improved local settings

--- local_settings.example/settings.py
+++ local_settings.example/settings.py
@@ -1,16 +1,14 @@
+"""GeoKey settings."""
+
 from geokey.core.settings.dev import *
 
-# Default email, used for automated correspondence
-# https://docs.djangoproject.com/en/1.8/ref/settings/#default-from-email
-DEFAULT_FROM_EMAIL = 'sender@example.com'
 
-# django-allauth setting, Determines the e-mail verification method during
-# signup.
-# see: http://django-allauth.readthedocs.org/en/latest/configuration.html
+# Default email, used for automated correspondence
+DEFAULT_FROM_EMAIL = 'sender@example.com'
+# django-allauth setting - determines the email verification method
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 # Database settings
-# see: https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
@@ -23,37 +21,32 @@ DATABASES = {
 }
 
 # Your server's secret key
-# see: https://docs.djangoproject.com/en/1.8/ref/settings/#std:setting-SECRET_KEY
 SECRET_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 
-# Google Analytics tracking code
+# Google Analytics tracking code (optional)
 GOOGLE_ANALYTICS = ''
 
-# Set this to true, if you want to enable video upload. Make sure you set the
-# Youtube settings
+# Set this to `True`, if you want to enable video upload to YouTube
 ENABLE_VIDEO = False
 
-# Youtube account settings. Need to be set if you enable video uploads.
-# Follow the guide to obtain the credentials: https://developers.google.com/youtube/registering_an_application
+# YouTube account settings (must be set if you enable video uploads)
+# https://developers.google.com/youtube/registering_an_application
 YOUTUBE_AUTH_EMAIL = 'your-email@example.com'
 YOUTUBE_AUTH_PASSWORD = 'password'
 YOUTUBE_DEVELOPER_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
-YOUTUBE_CLIENT_ID = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.apps.googleusercontent.com'
+YOUTUBE_CLIENT_ID = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.apps.googleusercontent.com'
 
-# Adds django packages to default INSTALLED_APPS, use it to register GeoKey
-# extensions
+# Additional Django packages and GeoKey extensions
 INSTALLED_APPS += (
-    # Add here (separated with commas)...
+    # ...
 )
 
-# Python path to WSGI application, used by Django's build in server
-# see: https://docs.djangoproject.com/en/1.8/ref/settings/#wsgi-application
-WSGI_APPLICATION = 'local_settings.wsgi.application'
-
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.8/howto/static-files/
-STATIC_ROOT = '/some/path/' # CHANGE THIS!
+STATIC_ROOT = '/some/path/'
 STATIC_URL = '/static/'
-
+# Media files (usually uploaded by the user)
 MEDIA_ROOT = normpath(join(dirname(dirname(abspath(__file__))), 'assets'))
 MEDIA_URL = '/assets/'
+
+# Python path to WSGI application
+WSGI_APPLICATION = 'local_settings.wsgi.application'
--- local_settings.example/wsgi.py
+++ local_settings.example/wsgi.py
@@ -1,13 +1,9 @@
-"""
-WSGI config for opencomap project.
+"""WSGI configuration."""
 
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
-"""
 import os
+
 from django.core.wsgi import get_wsgi_application
+
 
 try:
     import local_settings
@@ -15,14 +11,6 @@ try:
 except ImportError:
     settings_module = 'geokey.core.settings.project'
 
-# We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
-# if running multiple sites in the same mod_wsgi process. To fix this, use
-# mod_wsgi daemon mode with each site in its own daemon process, or use
-# os.environ["DJANGO_SETTINGS_MODULE"] = "dataService.settings"
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
 
-# This application object is used by any WSGI server configured to use this
-# file. This includes Django's development server, if the WSGI_APPLICATION
-# setting points here.
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 application = get_wsgi_application()
