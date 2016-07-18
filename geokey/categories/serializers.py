1222621e7d558ceae811b2cbd7766bd8d3a86d88
Julius O <j.osokinas@mappingforchange.org.uk>
2016-07-18 11:53:15 +0100

    Serializer cleanup for categories and fields
    Fields now include “is_displayfield” setting, to indicate whether the
    field is set as a display field for the category.

--- geokey/categories/serializers.py
+++ geokey/categories/serializers.py
@@ -5,99 +5,106 @@ from rest_framework.serializers import (
 )
 
 from geokey.core.serializers import FieldSelectorSerializer
-
-from .models import (
-    Category, Field, TextField, NumericField, LookupField, LookupValue,
-    MultipleLookupField, MultipleLookupValue
+from geokey.categories.models import (
+    Category, Field, TextField, NumericField,
+    LookupField, LookupValue, MultipleLookupField, MultipleLookupValue
 )
 
 
 class FieldSerializer(ModelSerializer):
-    """
-    Serializer for fields.
-    Used in .views.FieldApiDetail
-    """
+    """Serializer for fields."""
+
     fieldtype = ReadOnlyField()
+    is_displayfield = SerializerMethodField()
 
     class Meta:
+        """Meta information for serializer."""
+
         model = Field
         depth = 1
-        fields = (
-            'id', 'name', 'key', 'fieldtype', 'description', 'status',
-            'required', 'status'
-        )
+        fields = ('id', 'status', 'fieldtype', 'name', 'description', 'key',
+                  'required', 'is_displayfield')
         read_only_fields = ('id', 'name', 'key')
 
+    def get_is_displayfield(self, field):
+        """
+        Return whether the field is set as a display field for the category.
 
-class TextFieldSerializer(ModelSerializer):
-    """
-    Serializer for text fields.
-    Used in .views.FieldApiDetail
-    """
-    fieldtype = ReadOnlyField()
+        Parameters
+        ----------
+        field : geokey.categories.models.field
+            Field that is being serialized
+
+        Returns
+        -------
+        Boolean
+        """
+        if field.category.display_field == field:
+            return True
+        else:
+            return False
+
+
+class TextFieldSerializer(FieldSerializer):
+    """Serializer for text fields."""
 
     class Meta:
+        """Meta information for serializer."""
+
         model = TextField
         depth = 1
-        fields = (
-            'id', 'name', 'key', 'fieldtype', 'description', 'status',
-            'required', 'status', 'maxlength', 'textarea'
-        )
+        fields = ('id', 'status', 'fieldtype', 'name', 'description', 'key',
+                  'required', 'is_displayfield', 'maxlength', 'textarea')
         read_only_fields = ('id', 'name', 'key')
 
 
-class NumericFieldSerializer(ModelSerializer):
-    """
-    Serializer for numeric fields.
-    Used in .views.FieldApiDetail
-    """
-    fieldtype = ReadOnlyField()
+class NumericFieldSerializer(FieldSerializer):
+    """Serializer for numeric fields."""
 
     class Meta:
+        """Meta information for serializer."""
+
         model = NumericField
         depth = 1
-        fields = (
-            'id', 'name', 'key', 'fieldtype', 'description', 'status',
-            'required', 'minval', 'maxval'
-        )
+        fields = ('id', 'status', 'fieldtype', 'name', 'description', 'key',
+                  'required', 'is_displayfield', 'minval', 'maxval')
         read_only_fields = ('id', 'name', 'key')
 
 
 class LookupValueSerializer(ModelSerializer):
-    """
-    Serializer for lookup value.
-    Used in .views.FieldApiLookups
-    """
+    """Serializer for single lookupvalue."""
+
     class Meta:
+        """Meta information for serializer."""
+
         model = LookupValue
         fields = ('id', 'name', 'symbol')
 
 
 class MultipleLookupValueSerializer(ModelSerializer):
-    """
-    Serializer for lookup value.
-    Used in .views.FieldApiLookups
-    """
+    """Serializer for multiple lookup value."""
+
     class Meta:
+        """Meta information for serializer."""
+
         model = MultipleLookupValue
         fields = ('id', 'name', 'symbol')
 
 
 class BaseLookupSerializer(object):
-    """
-    Base class for LookupFieldSerializer and MultipleLookupFieldSerializer
-    """
+    """Base serializer for both single and multiple lookup fields."""
+
     def get_lookupvalues(self, field):
         """
-        Returns serialised Lookupvalues
+        Return serialized lookupvalues.
 
-        Parameter
-        ---------
+        Parameters
+        ----------
         field : geokey.categories.models.Field
-            Field which lookupvalues are serialised
+            Field of which lookupvalues are serialised
 
-        Return
-        ------
+        Returns
+        -------
         List
             Serialized lookupvalues
         """
@@ -111,69 +118,63 @@ class BaseLookupSerializer(object):
         return serializer.data
 
 
-class LookupFieldSerializer(ModelSerializer, BaseLookupSerializer):
-    """
-    Serializer for lookup fields.
-    Used in .views.FieldApiLookups
-    """
+class LookupFieldSerializer(FieldSerializer, BaseLookupSerializer):
+    """Serializer for single lookup fields."""
+
     lookupvalues = SerializerMethodField()
-    fieldtype = ReadOnlyField()
 
     class Meta:
+        """Meta information for serializer."""
+
         model = LookupField
         depth = 1
-        fields = (
-            'id', 'name', 'key', 'fieldtype', 'description', 'status',
-            'required', 'lookupvalues'
-        )
+        fields = ('id', 'status', 'fieldtype', 'name', 'description', 'key',
+                  'required', 'is_displayfield', 'lookupvalues')
         read_only_fields = ('id', 'name', 'key')
         write_only_fields = ('status',)
 
 
-class MultipleLookupFieldSerializer(ModelSerializer, BaseLookupSerializer):
-    """
-    Serializer for lookup fields.
-    Used in .views.FieldApiLookups
-    """
+class MultipleLookupFieldSerializer(FieldSerializer, BaseLookupSerializer):
+    """Serializer for multiple lookup fields."""
+
     lookupvalues = SerializerMethodField()
-    fieldtype = ReadOnlyField()
 
     class Meta:
+        """Meta information for serializer."""
+
         model = LookupField
         depth = 1
-        fields = (
-            'id', 'name', 'key', 'fieldtype', 'description', 'status',
-            'required', 'lookupvalues'
-        )
+        fields = ('id', 'status', 'fieldtype', 'name', 'description', 'key',
+                  'required', 'is_displayfield', 'lookupvalues')
         read_only_fields = ('id', 'name', 'key')
         write_only_fields = ('status',)
 
 
 class CategorySerializer(FieldSelectorSerializer):
-    """
-    Serializer for observation types. Used for AJAX API updates.
-    Used in .views.ObservationTypeAdminDetailView
-    """
+    """Serializer for categories."""
+
     fields = SerializerMethodField('get_fields_serialized')
 
     class Meta:
+        """Meta information for serializer."""
+
         model = Category
         depth = 1
-        fields = ('id', 'name', 'description', 'status', 'fields', 'colour',
-                  'created_at', 'symbol', 'order')
+        fields = ('id', 'status', 'created_at', 'name', 'description',
+                  'colour', 'symbol', 'order', 'fields')
         read_only_fields = ('id', 'name', 'created_at')
 
     def get_fields_serialized(self, category):
         """
-        Returns a list of serialized fields for the category
+        Return a list of serialized fields for the category.
 
-        Parameter
-        ---------
+        Parameters
+        ----------
         category : geokey.categories.models.category
-            Category which field are serialized
+            Category of which field are serialized
 
-        Return
-        ------
+        Returns
+        -------
         List
             Serialized fields
         """
