f7c7625fa88945aeaae5734012abb48820de0a14
Carles Boils Gisbert <c.gisbert@ucl.ac.uk>
2017-02-21 15:58:59 +0000

    Check if data has type

--- geokey/contributions/renderers/geojson.py
+++ geokey/contributions/renderers/geojson.py
@@ -17,9 +17,12 @@ class GeoJsonRenderer(BaseRenderer):
         """
         Renders a single `Contribution` into GeoJson
         """
-        data['type'] = "Feature"
-        data['geometry'] = json.loads(data.get('location').pop('geometry'))
-        return data
+        try:
+            data['type'] = "Feature"
+            data['geometry'] = json.loads(data.get('location').pop('geometry'))
+            return data
+        except:
+            return data
 
     def render_many(self, data):
         """
