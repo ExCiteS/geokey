088c709e50fdb224b2a61d3aec869a99b8ba5f34
Carles Boils Gisbert <c.gisbert@ucl.ac.uk>
2017-03-15 11:23:47 +0000

    add LoginrequiredMixin to LoggerList View

--- geokey/core/views.py
+++ geokey/core/views.py
@@ -5,6 +5,8 @@ from rest_framework.response import Response
 
 from django.views.generic import TemplateView
 
+from braces.views import LoginRequiredMixin
+
 from geokey.version import get_version
 from geokey.extensions.base import extensions
 from geokey.projects.views import ProjectContext
@@ -13,29 +15,32 @@ from geokey.core.models import LoggerHistory
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
-class LoggerList(ProjectContext, TemplateView):
+class LoggerList(LoginRequiredMixin, ProjectContext, TemplateView):
     """A list of all history logs."""
 
     template_name = 'logger/logger_list.html'
 
-    def get_context_data(self, *args, **kwargs):
-        """Return the context to render the view.
-        Overwrite the method to add the logs for the to the context.
+    def get_context_data(self, project_id, *args, **kwargs):
+        """
+        Return the context to render the view.
+        Parameters
+        ----------
+        project_id : int
+            Identifies the project in the database.
         Returns
         -------
         dict
             Context.
         """
 
-        # page = request.GET.get('page')
-
         context = super(LoggerList, self).get_context_data(
+            project_id,
             *args,
             **kwargs
         )
 
         logs = LoggerHistory.objects.filter(
-            project__contains={'id': str(context['project'].id)})
+            project__contains={'id': str(project_id)})
 
         context['logs'] = self.paginate_logs(
             logs[::-1],
