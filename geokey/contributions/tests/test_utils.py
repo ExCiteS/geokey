c25a8163969f932251bcebb5bf73546fb1d4a127
Carles Boils Gisbert <c.gisbert@ucl.ac.uk>
2017-03-28 11:00:35 +0100

    utils test file

---
+++ geokey/contributions/tests/test_utils.py
@@ -0,0 +1,78 @@
+"""Test all utils."""
+
+from importlib import import_module
+
+from django.test import TestCase
+from django.conf import settings
+from django.http import HttpRequest
+from django.conf import settings
+
+from oauth2client.client import OAuth2WebServerFlow
+
+from geokey.contributions.utils import (
+    my_flow_from_clientsecrets,
+    resumable_upload,
+    initialize_upload,
+    get_args
+)
+
+
+class GetArgsTest(TestCase):
+    """Test for method 'get_args'. """
+
+    def setUp(self):
+        """Set up tests. """
+        self.path = 'path/sample/test/test_file.mp4'
+        self.name = 'test_file'
+
+    def test_method(self):
+        """Test method. """
+        args = get_args(self.name, self.path)
+        args_var = vars(args)
+
+        self.assertEqual(args_var['file'], self.path)
+
+        self.assertEqual(args_var['title'], self.name)
+
+
+class MyFlowFromClientSecretTest(TestCase):
+    """Test for method 'my_flow_from_clientsecrets'. """
+
+    def setUp(self):
+        """Set up tests. """
+        self.youtube_uploader = {
+            'scope': "https://www.googleapis.com/auth/youtube.upload",
+            'auth_host_name': 'localhost',
+            'auth_host_port': [8080, 8000],
+            'credentials_path': '/vagrant/geokey/local_settings',
+            'credentials_file': "carabassa.json",
+            'client_info': {
+                "client_id": "109430273076-t3e30ie5aseb3laj2da0gkpikir6b0e9.apps.googleusercontent.com",
+                "client_secret": "o3U69gnO4FRipA1Q3K6gi0_N",
+                "redirect_uris": ["http://localhost"],
+                "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+                "token_uri": "https://accounts.google.com/o/oauth2/token"
+            },
+
+        }
+
+    def test_method(self):
+        """Test method. """
+        flow = my_flow_from_clientsecrets(
+            self.youtube_uploader['client_info'],
+            self.youtube_uploader['scope'])
+
+        client_info = self.youtube_uploader['client_info']
+
+        constructor_kwargs = {
+            'redirect_uri': None,
+            'auth_uri': client_info['auth_uri'],
+            'token_uri': client_info['token_uri'],
+            'login_hint': None,
+        }
+        flow_new = OAuth2WebServerFlow(
+            client_info['client_id'], client_info['client_secret'],
+            self.youtube_uploader['scope'], **constructor_kwargs)
+        self.assertEqual(flow.client_id, flow_new.client_id)
+        self.assertEqual(flow.scope, flow_new.scope)
+        self.assertEqual(flow.client_secret, flow_new.client_secret)
