fb58f3842750d9cafd29caf85b6024f9847912df
Carles Boils Gisbert <c.gisbert@ucl.ac.uk>
2017-05-23 14:01:59 +0100

    add new frequencies
    Add 5min, 10min, 20min, 30min

--- geokey/socialinteractions/base.py
+++ geokey/socialinteractions/base.py
@@ -4,4 +4,14 @@ from model_utils import Choices
 
 
 STATUS = Choices('active', 'inactive')
-FREQUENCY = Choices('weekly', 'monthly', 'daily', 'forthnightly', 'hourly')
+FREQUENCY = Choices(
+    '5min',
+    '10min',
+    '20min',
+    '30min',
+    'weekly',
+    'monthly',
+    'daily',
+    'forthnightly',
+    'hourly'
+)
