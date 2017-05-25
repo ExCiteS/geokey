d27596fba2c42b3ea836ed0f7b4482925825c284
Carles Boils Gisbert <c.gisbert@ucl.ac.uk>
2017-05-25 11:52:58 +0100

    test_utils
    -CheckDatesTest

---
+++ geokey/socialinteractions/tests/test_utils.py
@@ -0,0 +1,31 @@
+from django.test import TestCase
+
+from django.utils import timezone
+
+from geokey.socialinteractions.utils import (
+    start2pull,
+    check_dates,
+    create_new_observation,
+    get_category_and_field,
+    pull_from_social_media
+)
+
+from datetime import timedelta
+
+
+class CheckDatesTest(TestCase):
+    """Test for 'check_dates'."""
+
+    def test_method(self):
+        """Test method."""
+
+        now = timezone.now()
+        updated_at = now - timedelta(minutes=10)
+
+        value_true = check_dates(updated_at, '5min')
+
+        self.assertEqual(True, value_true)
+
+        value_false = check_dates(updated_at, '5min')
+
+        self.assertNotEqual(True, value_false)
