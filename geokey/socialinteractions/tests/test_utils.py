55e598905bbf93098c293247adce01693116319f
Julia Altenbuchner <julia.altenbuchner@ucl.ac.uk>
2017-12-18 10:58:55 +0000

    Tests updated

--- geokey/socialinteractions/tests/test_utils.py
+++ geokey/socialinteractions/tests/test_utils.py
@@ -12,7 +12,7 @@ from geokey.socialinteractions.utils import (
     get_category_and_field,
     pull_from_social_media
 )
-from geokey.categories.tests.model_factories import CategoryFactory, TextFieldFactory
+from geokey.categories.tests.model_factories import CategoryFactory, TextFieldFactory, NumericFieldFactory
 
 from geokey.users.tests.model_factories import UserFactory
 from geokey.projects.tests.model_factories import ProjectFactory
@@ -58,13 +58,18 @@ class GetCategoryAndFieldTest(TestCase):
         )
 
         self.field_text = TextFieldFactory.create(
-            name='tweet',
+            key='tweet',
+            category=self.category
+        )
+
+        self.tweet_id_field = NumericFieldFactory.create(
+            key='tweet-id',
             category=self.category
         )
 
     def test_method_when_check_when_category_and_field_exists(self):
         """Check method when category and field exists."""
-        tweet_cat, text_field = get_category_and_field(
+        tweet_cat, text_field, tweet_id_field = get_category_and_field(
             self.project,
             self.socialaccount)
 
@@ -72,14 +77,17 @@ class GetCategoryAndFieldTest(TestCase):
         self.assertEqual(tweet_cat.name, self.category.name)
 
         self.assertEqual(text_field.id, self.field_text.id)
-        self.assertEqual(text_field.name, self.field_text.name)
+        self.assertEqual(text_field.key, self.field_text.key)
+
+        self.assertEqual(tweet_id_field.id, self.tweet_id_field.id)
+        self.assertEqual(tweet_id_field.key, self.tweet_id_field.key)
 
     def test_method_when_check_when_category_and_field_does_not_exist(self):
         """Check method when category does not exist and field exists."""
         self.category.delete()
         self.category.save()
 
-        tweet_cat, text_field = get_category_and_field(
+        tweet_cat, text_field, tweet_id_field = get_category_and_field(
             self.project,
             self.socialaccount)
 
@@ -87,7 +95,10 @@ class GetCategoryAndFieldTest(TestCase):
         self.assertEqual(tweet_cat.name, self.category.name)
 
         self.assertNotEqual(text_field.id, self.field_text.id)
-        self.assertEqual(text_field.name, self.field_text.name)
+        self.assertEqual(text_field.key, self.field_text.key)
+
+        self.assertNotEqual(tweet_id_field.id, self.tweet_id_field.id)
+        self.assertEqual(tweet_id_field.key, self.tweet_id_field.key)
 
 
 class CreateNewObservationTest(TestCase):
@@ -107,7 +118,12 @@ class CreateNewObservationTest(TestCase):
         )
 
         self.field_text = TextFieldFactory.create(
-            name='tweet',
+            key='tweet',
+            category=self.category
+        )
+
+        self.tweet_id_field = NumericFieldFactory.create(
+            key='tweet-id',
             category=self.category
         )
 
@@ -131,7 +147,8 @@ class CreateNewObservationTest(TestCase):
             self.si_pull,
             self.geo_tweet,
             self.category,
-            self.field_text
+            self.field_text,
+            self.tweet_id_field
         )
 
         self.assertEqual(init_obs + 1, Observation.objects.count())
