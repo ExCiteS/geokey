e92c742e32f2bb3e38fd6a38acfdc419ee50f5d4
JamesBradbury <github@jamesbradbury.co.uk>
2018-03-12 18:28:38 +0000

    Docker db paths udpated to fix issue relating to updated docker/postgres Extra blank line added for PEP-8.

--- geokey/projects/base.py
+++ geokey/projects/base.py
@@ -4,5 +4,6 @@ from model_utils import Choices
 
 from local_settings.settings import ALLOWED_CONTRIBUTORS
 
+
 STATUS = Choices('active', 'inactive', 'deleted')
 EVERYONE_CONTRIBUTES = Choices(*ALLOWED_CONTRIBUTORS)
