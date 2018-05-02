c778be3ff56b7b17d43ec523598534a22c65088e
JamesBradbury <github@jamesbradbury.co.uk>
2018-05-02 17:25:26 +0100

    Simplify migrations hoping to keep both local and Travis-CI tests happy.

--- geokey/users/migrations/0002_auto_20150904_1113.py
+++ geokey/users/migrations/0002_auto_20150904_1113.py
@@ -7,7 +7,7 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('users', '0001_manual'),
+        ('users', '0001_initial'),
     ]
 
     operations = [
