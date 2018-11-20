9f9b7de24c2505aa6b55b8005e257dc9b89e01a4
Julius Osokinas <julius@osokinas.com>
2018-11-20 18:45:08 +0000

    Add (and test) support for AC3

--- geokey/contributions/base.py
+++ geokey/contributions/base.py
@@ -10,7 +10,7 @@ COMMENT_REVIEW = Choices('open', 'resolved')
 MEDIA_STATUS = Choices('active', 'deleted')
 
 ACCEPTED_IMAGE_FORMATS = ('png', 'jpeg', 'gif')
-ACCEPTED_AUDIO_FORMATS = ('wav', 'wave', 'mp3', 'mpeg', '3gpp', '3gpp2', 'x-m4a', 'mp4', 'ogg', 'amr', 'flac')
+ACCEPTED_AUDIO_FORMATS = ('wav', 'wave', 'mp3', 'mpeg', '3gpp', '3gpp2', 'x-m4a', 'mp4', 'ogg', 'amr', 'flac', 'ac3')
 ACCEPTED_VIDEO_FORMATS = (
     'mpeg', 'mpeg2', 'mp4', 'quicktime', '3gpp', '3gpp2', 'avi', 'x-flv',
     'x-matroska', 'x-msvideo', 'x-ms-wmv', 'ogg', 'webm'
