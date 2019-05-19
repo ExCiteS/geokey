6d8dc771f2da8ff162a93564ec4a495f48bb4875
Julius Osokinas <julius@osokinas.com>
2019-05-19 13:41:36 +0100

    Improve command for checking whether user has confirmed email address

--- geokey/users/tests/test_commands.py
+++ geokey/users/tests/test_commands.py
@@ -16,38 +16,80 @@ class CheckConfirmTest(TestCase):
     def test_check_confirm(self):
         confirmed_user = UserFactory.create()
         unconfirmed_user = UserFactory.create()
+        unconfirmed_user_without_confirmation = UserFactory.create()
+        inactive_user = UserFactory.create(**{'is_active': True})
+        unconfirmed_superuser = UserFactory.create(**{'is_superuser': True})
 
-        confirmed_email = EmailAddress.objects.create(
+        confirmed_user_email = EmailAddress.objects.create(
             user=confirmed_user,
             email=confirmed_user.email,
             verified=True
         )
-
-        unconfirmed_email = EmailAddress.objects.create(
+        unconfirmed_user_email = EmailAddress.objects.create(
             user=unconfirmed_user,
             email=unconfirmed_user.email,
             verified=False
         )
+        unconfirmed_inactive_email = EmailAddress.objects.create(
+            user=inactive_user,
+            email=inactive_user.email,
+            verified=False
+        )
+        unconfirmed_superuser_email = EmailAddress.objects.create(
+            user=unconfirmed_superuser,
+            email=unconfirmed_superuser.email,
+            verified=False
+        )
+
+        # Edge case - email confirmation was not sent
+        EmailAddress.objects.create(
+            user=unconfirmed_user_without_confirmation,
+            email=unconfirmed_user_without_confirmation.email,
+            verified=False
+        )
 
         EmailConfirmation.objects.create(
-            email_address=confirmed_email,
+            email_address=confirmed_user_email,
             created=datetime.utcnow().replace(tzinfo=utc),
             sent=datetime.utcnow().replace(tzinfo=utc),
-            key='38974hewjkhnuweyf8h8d'
+            key='a0qzf3xwn420gdbv2d1v'
         )
         EmailConfirmation.objects.create(
-            email_address=unconfirmed_email,
+            email_address=unconfirmed_user_email,
             created=datetime.utcnow().replace(tzinfo=utc),
             sent=datetime.utcnow().replace(tzinfo=utc),
-            key='38974hewjkhnsdfuweyf8h8d'
+            key='16fy83dz7ucb6p8ogsc8'
+        )
+        EmailConfirmation.objects.create(
+            email_address=unconfirmed_inactive_email,
+            created=datetime.utcnow().replace(tzinfo=utc),
+            sent=datetime.utcnow().replace(tzinfo=utc),
+            key='qogd4jgvu7x7zi1eic2x'
+        )
+        EmailConfirmation.objects.create(
+            email_address=unconfirmed_inactive_email,
+            created=datetime.utcnow().replace(tzinfo=utc),
+            sent=datetime.utcnow().replace(tzinfo=utc),
+            key='ei8n5addsmu5igtvgie1'
+        )
+        EmailConfirmation.objects.create(
+            email_address=unconfirmed_superuser_email,
+            created=datetime.utcnow().replace(tzinfo=utc),
+            sent=datetime.utcnow().replace(tzinfo=utc),
+            key='5g3ovaztqun1b8c7wgjn'
         )
 
         yesterday = datetime.utcnow().replace(tzinfo=utc)
         command = CheckConfirm()
         command.set_user_inactive(yesterday)
 
-        ref_unconfirmed = User.objects.get(pk=unconfirmed_user.id)
-        self.assertFalse(ref_unconfirmed.is_active)
-
-        ref_confirmed = User.objects.get(pk=confirmed_user.id)
-        self.assertTrue(ref_confirmed.is_active)
+        confirmed_user.refresh_from_db()
+        self.assertTrue(confirmed_user.is_active)
+        unconfirmed_user.refresh_from_db()
+        self.assertFalse(unconfirmed_user.is_active)
+        unconfirmed_user_without_confirmation.refresh_from_db()
+        self.assertFalse(unconfirmed_user_without_confirmation.is_active)
+        inactive_user.refresh_from_db()
+        self.assertFalse(inactive_user.is_active)
+        unconfirmed_superuser.refresh_from_db()
+        self.assertTrue(unconfirmed_superuser.is_active)
